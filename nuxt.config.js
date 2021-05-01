d7c37753482f4c44ff0f515cec2e032fdb59a44b
Sergi Mateo <sergimateo@gmail.com>
2021-05-01 17:09:51 +0200

    finished app v1.0

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/data.js',
+    '~/plugins/data/data.js',
     '~/plugins/ApiMarvelPlaceholder.js',
     '~/plugins/MarvelPlaceholderService.js',
   ],
--- plugins/data.js
+++
@@ -1,4 +0,0 @@
-export default {
-  ts: '1',
-  apiKey: '5048b649ff1da6422ad9039248e892fe',
-}
